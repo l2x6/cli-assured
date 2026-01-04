bbf9fe5647297a62ce13871ff6f0c2ab3f0151e9
Peter Palaga <ppalaga@redhat.com>
2026-01-04 16:07:08 +0100

    Maven testing harness

---
+++ mvn-assured/src/test/java/org/l2x6/mvn/assured/MvnTest.java
@@ -0,0 +1,20 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 CLI Assured contributors as indicated by the @author tags
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.l2x6.mvn.assured;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MvnTest {
+    @Test
+    void hashString() {
+        Assertions
+                .assertThat(
+                        Mvn.hashString(
+                                "https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/3.9.11/apache-maven-3.9.11-bin.zip"))
+                .isEqualTo("a2d47e15");
+    }
+
+}
